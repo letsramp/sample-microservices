c45568d01f12caebaddd2196decc3d303b505636
pellepedro <Per.Pettersson@gmail.com>
2022-09-05 15:56:14 -0400

    paymentservice uses http insteadof https

--- src/paymentservice/rest-server.js
+++ src/paymentservice/rest-server.js
@@ -1,11 +1,5 @@
 const {parentPort, data} = require("worker_threads");
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('cert/key.pem', 'utf8');
-var certificate = fs.readFileSync('cert/cert.pem', 'utf8');
 const charge = require('./charge');
-var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
 app.use(express.json())
@@ -15,7 +9,7 @@ app.post('/charge', function(req, res){
 })
 
 restPort   = process.env.REST_PORT;
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-console.log("Rest Server running on port: " + restPort);
-httpsServer.listen(restPort);
+
+app.listen(restPort, () => {
+  console.log(`Rest server started on on port ${restPort}`)
+})
