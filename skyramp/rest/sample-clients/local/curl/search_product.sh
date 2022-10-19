e7c4f4ea89cc07ba7e89ff4a7b8b9dcd15cbb6e7
nithinmichael <nithin@mode.net>
2022-10-19 08:43:55 -0700

    Update search_product.sh
    Signed-off-by: nithinmichael <nithin@mode.net>

--- skyramp/rest/sample-clients/local/curl/search_product.sh
+++ skyramp/rest/sample-clients/local/curl/search_product.sh
@@ -5,4 +5,4 @@ set -e
 curl -X 'GET' \
   'http://product-catalog-service-port60000.demo.skyramp.test/search-products?query=kitchen' \
     --resolve product-catalog-service-port60000.demo.skyramp.test:80:127.0.0.1 \
-  -H 'accept: application/json' | jq .
+  -H 'accept: application/json'
