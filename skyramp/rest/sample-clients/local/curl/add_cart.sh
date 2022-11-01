2789de5a827a757a0a78485302b18e5bdb17fe15
pellepedro <Per.Pettersson@gmail.com>
2022-10-31 22:01:30 -0400

    fixed path parameter for cart- and checkout service
    Signed-off-by: pellepedro <Per.Pettersson@gmail.com>

--- skyramp/rest/sample-clients/local/curl/add_cart.sh
+++ skyramp/rest/sample-clients/local/curl/add_cart.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-curl -X 'POST' 'http://cart-service-port60000.demo.skyramp.test/cart/abcde' \
+curl -X 'POST' 'http://cart-service-port60000.demo.skyramp.test/cart/user_id/abcde' \
   --resolve cart-service-port60000.demo.skyramp.test:80:127.0.0.1 \
   -H 'accept: application/json' \
   -H 'content-type: application/json' \
