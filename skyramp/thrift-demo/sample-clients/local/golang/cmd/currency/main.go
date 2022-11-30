8924a82dabf39e9f94d9f118b914436bb1966db6
pellepedro <Per.Pettersson@gmail.com>
2022-11-30 12:13:06 -0500

    renamed project folders and targets
    - Renamed project folders to grpc-demo, thrift-demo, rest-demo
    - Renamed targets to e2e-target
    - Added project name to ingress fqdn
    - Removed Authority header to use local dnsmasq to resolve fqdn
    
    Signed-off-by: pellepedro <Per.Pettersson@gmail.com>

---
+++ skyramp/thrift-demo/sample-clients/local/golang/cmd/currency/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	api "sample-thrift/demo"
+)
+
+const (
+	thriftHttpPath = "/CurrencyService"
+)
+
+func main() {
+	clientAddr := "thrift-demo.currency-service-port50000.e2e-target.skyramp.test"
+	opt := NewDefaultOption()
+	opt.HttpUrl = thriftHttpPath
+
+	c, trans, err := NewThriftClient(clientAddr, opt)
+	if err != nil {
+		fmt.Printf("Failed to connect to server: %v", err)
+		os.Exit(1)
+	}
+	err = trans.Open()
+	if err != nil {
+		fmt.Printf("Failed to connect to Currency Service: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println("connected to the currency service")
+	client := api.NewCurrencyServiceClient(c)
+	fmt.Println("About to get list of currencies")
+	currencies, err := client.GetSupportedCurrencies(context.Background())
+	if err != nil {
+		fmt.Printf("Failed to get list of supported currencies: %v", err)
+		os.Exit(1)
+	}
+	fmt.Printf("\nsupported currencies\n%v", currencies)
+
+	fmt.Println("\nAbout to convert USD to CAD")
+	result, err := client.Convert(context.Background(),
+		&api.Money{
+			CurrencyCode: "USD",
+			Units:        int64(100),
+			Nanos:        int32(99000000),
+		}, "CAD")
+	if err != nil {
+		fmt.Printf("Failed to convert currencies: %v", err)
+		os.Exit(1)
+	}
+	fmt.Printf("\nSuccessfully converted USD to CAD: %v\n", result)
+}
