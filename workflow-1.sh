9f20222d95c64ce3ea7b3aa9ff3f6b5695b767e5
pellepedro <Per.Pettersson@gmail.com>
2023-08-21 14:04:48 +0000

    removed kubernetes assets from devcontainer.json

--- workflow-1.sh
+++ workflow-1.sh
@@ -4,7 +4,8 @@ cd /workspaces/sample-microservices/skyramp/docker-compose
 
 docker pull public.ecr.aws/j1n2c2p2/rampup/worker:latest
 docker pull public.ecr.aws/j1n2c2p2/microservices-demo/productcatalogservice:v0.3.9
-docker pull public.ecr.aws/j1n2c2p2/microservices-demo/checkoutservice:v0.4.0 
+docker pull public.ecr.aws/j1n2c2p2/microservices-demo/checkoutservice:v0.4.1
 docker pull public.ecr.aws/j1n2c2p2/microservices-demo/cartservice:v0.3.9
 
 docker compose up
+
