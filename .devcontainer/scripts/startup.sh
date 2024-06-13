1f121bd83c09c84910faf9508949bd35c869cc62
Shiny Chimra <shinychimra07@gmail.com>
2024-06-13 11:32:26 -0700

    test

--- .devcontainer/scripts/startup.sh
+++ .devcontainer/scripts/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eux
 
-# Bringup sample-microservices
-docker-compose -f src/docker-compose.yml up -d
-
 # Bringup Skyramp
-echo $ECR_TOKEN | docker login --username AWS --password-stdin 296613639307.dkr.ecr.us-west-2.amazonaws.com 
+# echo $ECR_TOKEN | docker login --username AWS --password-stdin 296613639307.dkr.ecr.us-west-2.amazonaws.com 
 docker-compose -f /etc/skyramp/skyramp-docker-compose.yaml up -d
+
+# Bringup sample-microservices
+docker-compose -f ../../workspaces/sample-microservices/src/docker-compose.yml up -d
