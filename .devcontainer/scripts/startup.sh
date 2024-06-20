ce47f7413e1a97fe4c84746cf7e973278ec814b8
Shiny Chimra <shinychimra07@gmail.com>
2024-06-20 09:20:29 -0700

    test

--- .devcontainer/scripts/startup.sh
+++ .devcontainer/scripts/startup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -eux
 
-echo "cs name =============="
-echo $SKY_CODESPACE_NAME
-
-echo "new password =============="
-echo $NEW_PASSWORD
-
-"vscode:skyramp" | sudo chpasswd
-
 # Bringup Skyramp
 # echo $ECR_TOKEN | docker login --username AWS --password-stdin 296613639307.dkr.ecr.us-west-2.amazonaws.com 
 docker-compose -f /etc/skyramp/skyramp-docker-compose.yaml up -d
