17b1542ec80e8c98a1e04e928cd58dec50dc98f0
Shiny Chimra <shinychimra07@gmail.com>
2024-06-27 22:30:30 -0700

    test

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -1,4 +1,4 @@
 gh codespace ports visibility 4000:public -c $CODESPACE_NAME
 # Start Skyramp
-cd $CODESPACE_VSCODE_FOLDER/skyramp-server && npm i && npm run start &
+cd /workspaces/sample-microservices/skyramp-server && npm i && npm run start &
 docker compose -f /workspaces/sample-microservices/skyramp/docker/demo/docker-compose.yml up -d --wait || true
