0380aacc53aca6814098034cd00ff229736dd099
Shiny Chimra <shinychimra07@gmail.com>
2024-06-27 19:19:51 -0700

    test

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -1,5 +1,2 @@
-docker compose -f /workspaces/sample-microservices/skyramp/docker/demo/docker-compose.yml up -d --wait || true
 gh codespace ports visibility 4000:public -c $CODESPACE_NAME
-cd skyramp-server
-npm i 
-npm run start
+docker compose -f /workspaces/sample-microservices/skyramp/docker/demo/docker-compose.yml up -d --wait || true
