a1c4afd7d93cf77cd8bd9ab090e6f6848b0c292e
Shiny Chimra <shinychimra07@gmail.com>
2024-06-29 11:16:59 -0700

    test

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-gh codespace ports visibility 4000:public -c $CODESPACE_NAME
-docker compose -f /workspaces/sample-microservices/skyramp/docker/demo/docker-compose.yml up -d --wait || true
-#install skyramp cli curl -fsSL https://skyramp.dev/installer.sh
-curl -fsSL https://skyramp.dev/installer.sh | bash
-#run skyramp cli
+# gh codespace ports visibility 4000:public -c $CODESPACE_NAME
+# docker compose -f /workspaces/sample-microservices/skyramp/docker/demo/docker-compose.yml up -d --wait || true
+# #install skyramp cli curl -fsSL https://skyramp.dev/installer.sh
+# curl -fsSL https://skyramp.dev/installer.sh | bash
+# #run skyramp cli
 skyramp --version
