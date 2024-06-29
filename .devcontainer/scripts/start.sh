b5a2305ec399dfdeb285cd634a8767533b4dfd02
Shiny Chimra <shinychimra07@gmail.com>
2024-06-29 07:43:58 -0700

    update to start

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 gh codespace ports visibility 4000:public -c $CODESPACE_NAME
-# Start Skyramp
-skyramp server up > output.log 2>&1 & || true
 docker compose -f /workspaces/sample-microservices/skyramp/docker/demo/docker-compose.yml up -d --wait || true
+#install skyramp cli curl -fsSL https://skyramp.dev/installer.sh
+curl -fsSL https://skyramp.dev/installer.sh | bash
+#run skyramp cli
+skyramp --version
