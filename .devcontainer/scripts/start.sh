4c8582ee20abed430f310e636f13fbdb32314aea
Shiny Chimra <shinychimra07@gmail.com>
2024-07-01 11:45:22 -0700

    public port for dashboard

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -5,6 +5,9 @@ docker compose -f /workspaces/sample-microservices/skyramp/docker/demo/docker-co
 nohup skyramp server up -vvv &
 # open public port for skyramp server
 gh codespace ports visibility 45132:public -c $CODESPACE_NAME
+# open public port for skyramp dashboard
+gh codespace ports visibility 4000:public -c $CODESPACE_NAME
+gh codespace ports visibility 3000:public -c $CODESPACE_NAME
 # install python3.11 and requirements
 apt-get update && apt-get install -y python3.11 python3-pip && update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
 pip3 install -r /etc/skyramp/requirements.txt
