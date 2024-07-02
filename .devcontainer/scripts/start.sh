06ef4cc7cd103c2f909105cf6e43222c2805de64
Shiny Chimra <shinychimra07@gmail.com>
2024-07-01 17:13:08 -0700

    temp fix for docker img late limit

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # install sample-microservices and skyramp
-docker compose -f /workspaces/sample-microservices/skyramp/docker/demo/docker-compose.yml up -d --wait
+docker compose -f /workspaces/sample-microservices/skyramp/docker/demo/docker-compose.yml up -d --wait || true
 # start skyramp  server
 nohup skyramp server up -vvv &
 # open public port for skyramp server
 gh codespace ports visibility 45132:public -c $CODESPACE_NAME
 # open public port for skyramp dashboard
-gh codespace ports visibility 4000:public -c $CODESPACE_NAME
-gh codespace ports visibility 3000:public -c $CODESPACE_NAME
+gh codespace ports visibility 4000:public -c $CODESPACE_NAME || true
+gh codespace ports visibility 3000:public -c $CODESPACE_NAME || true
 # install python3.11 and requirements
 apt-get update && apt-get install -y python3.11 python3-pip && update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
 pip3 install -r /etc/skyramp/requirements.txt
