485323f4e8cbf715953d90887d41ba8de17f67e2
Shiny Chimra <shinychimra07@gmail.com>
2024-07-17 14:17:47 -0700

    update

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -4,8 +4,8 @@ docker compose -f /workspaces/sample-microservices/docker-compose.yml up -d --wa
 # start skyramp  server
 nohup skyramp server up -vvv &
 # open public port for skyramp dashboard
-gh codespace ports visibility 4000:public -c $CODESPACE_NAME || true
-gh codespace ports visibility 3000:public -c $CODESPACE_NAME || true
+gh codespace ports visibility 4000:public -c $CODESPACE_NAME
+gh codespace ports visibility 3000:public -c $CODESPACE_NAME
 # install python3.11 and requirements
 apt-get update && apt-get install -y python3.11 python3-pip && update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
 pip3 install -r /etc/skyramp/requirements.txt
