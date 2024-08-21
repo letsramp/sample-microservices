408014146261ad0d6f845ed8c2967083c7e5e4c8
Shiny Chimra <shinychimra07@gmail.com>
2024-08-21 10:20:13 -0700

    initial commit

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# install skyramp
+bash -c "$(curl -fsSL https://skyramp.dev/installer.sh)"
 # install sample-microservices and skyramp
 docker compose -f /workspaces/sample-microservices/skyramp/docker/demo/docker-compose.yml up -d --wait || true
 # start skyramp  server
@@ -8,4 +10,4 @@ gh codespace ports visibility 4000:public -c $CODESPACE_NAME
 gh codespace ports visibility 3000:public -c $CODESPACE_NAME
 # install python3.11 and requirements
 apt-get update && apt-get install -y python3.11 python3-pip && update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
-pip3 install -r /etc/skyramp/requirements.txt
+pip3 install -r /workspaces/sample-microservices/.devcontainer/scripts/requirements.txt
