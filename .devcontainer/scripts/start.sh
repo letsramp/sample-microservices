cdbe6056c5f1a5b0f42ca710af81d4a1b295ea6b
Shiny Chimra <shinychimra07@gmail.com>
2024-08-22 15:06:19 -0700

    cleanup

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-echo "Starting Skyramp ............. ${PYTHON_PIP_PATH} hello====================="
+
 # install skyramp
 yes | bash -c "$(curl -fsSL https://skyramp.dev/installer.sh)"
 
 # install skyramp worker and dashboard
 curl -o /etc/skyramp/docker-compose.yml https://raw.githubusercontent.com/letsramp/sample-microservices/script_branch/.devcontainer/scripts/docker-compose.yml
 docker compose -f /etc/skyramp/docker-compose.yml up -d --wait || true
-# skyramp dashboard up -d -vvv || true
 
 # start skyramp  server
 nohup skyramp server up -vvv &
-
-# # open public port for skyramp dashboard
-# gh codespace ports visibility 4000:public -c $CODESPACE_NAME
-# gh codespace ports visibility 3000:public -c $CODESPACE_NAME
 
 # install python requirements
 curl -o /etc/skyramp/requirements.txt https://raw.githubusercontent.com/letsramp/sample-microservices/script_branch/.devcontainer/scripts/requirements.txt
