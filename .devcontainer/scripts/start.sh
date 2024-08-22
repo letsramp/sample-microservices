f66ecb7e4b36c5d10a62504ec37cc660b48ffb67
Shiny Chimra <shinychimra07@gmail.com>
2024-08-22 08:16:15 -0700

    test commit

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -11,7 +11,7 @@ nohup skyramp server up -vvv &
 gh codespace ports visibility 4000:public -c $CODESPACE_NAME
 gh codespace ports visibility 3000:public -c $CODESPACE_NAME
 # install python3.11 and requirements
-apt-get update && apt-get install -y python3.11 python3-pip && update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
+# apt-get update && apt-get install -y python3.11 python3-pip && update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
 curl -o /etc/skyramp/requirements.txt https://raw.githubusercontent.com/letsramp/sample-microservices/script_branch/.devcontainer/scripts/requirements.txt
 chmod +x /etc/skyramp/requirements.txt
-pip install -r /etc/skyramp/requirements.txt
+# pip install -r /etc/skyramp/requirements.txt
