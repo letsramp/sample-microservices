49ca9e4f07b32b3a54f036225f8f7b0c81229808
Shiny Chimra <shinychimra07@gmail.com>
2024-08-21 22:34:32 -0700

    refactor

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -2,7 +2,7 @@
 # install skyramp
 yes | bash -c "$(curl -fsSL https://skyramp.dev/installer.sh)"
 # install skyramp worker and dashboard
-# curl -o https://raw.githubusercontent.com/letsramp/sample-microservices/script_branch/.devcontainer/scripts/docker-compose.yml /etc/skyramp/docker-compose.yml
+# curl -o /etc/skyramp/docker-compose.yml https://raw.githubusercontent.com/letsramp/sample-microservices/script_branch/.devcontainer/scripts/docker-compose.yml
 # docker compose -f /etc/skyramp/docker-compose.yml up -d --wait || true
 skyramp dashboard up -d -vvv || true
 # start skyramp  server
@@ -12,6 +12,6 @@ gh codespace ports visibility 4000:public -c $CODESPACE_NAME
 gh codespace ports visibility 3000:public -c $CODESPACE_NAME
 # install python3.11 and requirements
 apt-get update && apt-get install -y python3.11 python3-pip && update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
-curl -o https://raw.githubusercontent.com/letsramp/sample-microservices/script_branch/.devcontainer/scripts/requirements.txt /etc/skyramp/requirements.txt
+curl -o /etc/skyramp/requirements.txt https://raw.githubusercontent.com/letsramp/sample-microservices/script_branch/.devcontainer/scripts/requirements.txt
 chmod +x /etc/skyramp/requirements.txt
 pip install -r /etc/skyramp/requirements.txt
