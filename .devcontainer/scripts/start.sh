d8c1609a84147802085bfaf07cba219b85c28c58
Shiny Chimra <shinychimra07@gmail.com>
2024-08-22 08:30:09 -0700

    commenting gh cmd

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -7,9 +7,9 @@ yes | bash -c "$(curl -fsSL https://skyramp.dev/installer.sh)"
 skyramp dashboard up -d -vvv || true
 # start skyramp  server
 nohup skyramp server up -vvv &
-# open public port for skyramp dashboard
-gh codespace ports visibility 4000:public -c $CODESPACE_NAME
-gh codespace ports visibility 3000:public -c $CODESPACE_NAME
+# # open public port for skyramp dashboard
+# gh codespace ports visibility 4000:public -c $CODESPACE_NAME
+# gh codespace ports visibility 3000:public -c $CODESPACE_NAME
 # install python3.11 and requirements
 # apt-get update && apt-get install -y python3.11 python3-pip && update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
 curl -o /etc/skyramp/requirements.txt https://raw.githubusercontent.com/letsramp/sample-microservices/script_branch/.devcontainer/scripts/requirements.txt
